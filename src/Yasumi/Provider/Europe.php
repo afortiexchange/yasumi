ad45edfeb4a0640ee4885ee7badd6b8393f718be
Dominik Piłat <dominik.pilat@afortiexchange.pl>
2019-08-19 08:16:00 +0200

    EU

---
+++ src/Yasumi/Provider/Europe.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dominik
+ * Date: 09.10.2018
+ * Time: 14:29
+ */
+
+namespace Yasumi\Provider;
+
+use Yasumi\Holiday;
+
+class Europe extends AbstractProvider
+{
+
+    use CommonHolidays, ChristianHolidays;
+
+    /**
+     * Code to identify this Holiday Provider. Typically this is the ISO3166 code corresponding to the respective
+     * country or sub-region.
+     */
+    const ID = 'EU';
+
+    /**
+     * Initialize holidays for Europe.
+     *
+     * @throws \InvalidArgumentException
+     * @throws \Yasumi\Exception\UnknownLocaleException
+     * @throws \Exception
+     */
+    public function initialize()
+    {
+        $this->timezone = 'Europe/Brussels';
+
+        // Add common holidays
+        $this->addHoliday($this->newYearsDay($this->year, $this->timezone, $this->locale));
+        $this->addHoliday($this->internationalWorkersDay($this->year, $this->timezone, $this->locale));
+
+        $this->addHoliday($this->goodFriday($this->year, $this->timezone, $this->locale));
+        $this->addHoliday($this->easterMonday($this->year, $this->timezone, $this->locale, Holiday::TYPE_BANK));
+        $this->addHoliday($this->christmasDay($this->year, $this->timezone, $this->locale));
+        $this->addHoliday($this->secondChristmasDay($this->year, $this->timezone, $this->locale));
+
+    }
+}
